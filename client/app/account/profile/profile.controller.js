08d9bb187bdc7538c3f9cf7e5562e9d3ad7d75fe
Dan Zhang <dyz2@duke.edu>
2015-04-14 02:03:14 -0400

    Profile page design

--- client/app/account/profile/profile.controller.js
+++ client/app/account/profile/profile.controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('gratitudeApp')
-  .controller('ProfileCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+.controller('ProfileCtrl', function ($scope, $modal) {
+	$scope.posts = [
+	{title: 'post 1', body: 5},
+	{title: 'post 2', body: 2},
+	{title: 'post 3', body: 15},
+	{title: 'post 4', body: 9},
+	{title: 'post 5', body: 4}
+	];
+
+	$scope.addPost = function(){
+		var date = new Date;
+		var options = { month: 'long', day: 'numeric', year: 'numeric' }
+		$scope.posts.push({title: date.toLocaleDateString('en-US', options), body: 'Lol'});
+	};
+});
